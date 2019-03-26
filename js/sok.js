00cb1f665a4d5ac8d229a6b5a37c0423a7f3e15e
Artyom <taranenko@grechka.digital>
2019-03-26 16:56:04 +0300

    hove do

--- js/sok.js
+++ js/sok.js
@@ -1,10 +1,81 @@
-// $(function () {
-// 	$('.hour-block').on({
-// 		mouseenter: function() {
-// 			$('.timeline').addClass('display');
-// 		},
-// 		mouseleave: function() {
-// 			$('.timeline').removeClass('display');
-// 		}
-// 	});
-// });
+window.onload = function() {
+
+	var timeline = document.querySelector('.timeline');
+	var dateBlock = document.querySelector('.date-block');
+	var hourBlockLength = document.querySelectorAll('.hour-block').length;
+	var hourBlock = document.querySelector('.hour-block');
+	var timelineLeft = document.querySelector('.timeline__left');
+	var timelineRight = document.querySelector('.timeline__right');
+	const amountHourBlock = hourBlock.offsetWidth * hourBlockLength;
+	var onRightEdge, onLeftEdge;
+	const step = 30;
+
+	var state = {
+		left: 0,
+		width: 30
+	}
+
+	// function widthTimeline() {
+	// 	width = timeline.width();
+	// 	state.width = width;
+	// 	console.log(width, state.width);
+	// }
+	// widthTimeline();
+	// $('.date-block').on({
+	// 	mouseenter: function() {
+	// 		$('.timeline').addClass('display');
+	// 	},
+	// 	mouseleave: function() {
+	// 		$('.timeline').removeClass('display');
+	// 	}
+	// });
+
+	// timeline.onmousedown = function(e) {
+	// 	var timelineCoords = getCoords(timeline);
+	// 	var shiftX = e.pageX - timelineCoords.left;
+
+	// 	var dateBlockCoords = getCoords(dateBlock);
+	// 	document.onmousemove = function(e) {
+	// 		var newLeft = e.pageX - shiftX - dateBlockCoords.left;
+
+	// 		// курсор ушёл вне блока
+	// 		if (newLeft < 0) {
+	// 			newLeft = 0;
+	// 		}
+	// 		var rightEdge = amountHourBlock - timeline.offsetWidth;
+	// 		if (newLeft > rightEdge) {
+	// 			newLeft = rightEdge;
+	// 		}
+
+	// 		timeline.style.left = newLeft + 'px';
+	// 	}
+	// 	document.onmouseup = function() {
+	// 		document.onmousemove = document.onmouseup = null;
+	// 	};
+
+	// 	return false; // disable selection start (cursor change)
+	// };
+	timelineRight.onmousedown = function(e) {
+		var timelineCoords = getCoords(timeline);
+		var dateBlockCoords = getCoords(dateBlock);
+		var shiftX = e.pageX - timelineCoords.left;
+		document.onmousemove = function(e) {
+			var newLeft = e.pageX - shiftX - dateBlockCoords.left;
+			console.log(newLeft, timeline.offsetWidth);
+			var LeftEdge = timeline.offsetWidth + newLeft;
+			timeline.style.width = LeftEdge + 'px';
+		}
+	};
+	document.onmouseup = function() {
+		document.onmousemove = document.onmouseup = null;
+	};
+	timeline.ondragstart = function() {
+		return false;
+	};
+	function getCoords(e) {
+		var box = e.getBoundingClientRect();
+		return {
+			left: box.left + pageXOffset
+		};
+	}
+}
